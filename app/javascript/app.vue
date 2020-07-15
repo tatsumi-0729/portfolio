5350faa83af88ad963ac81b41bef92d646d848eb
tatsumi <tamura.tatsumii@gmail.com>
2020-07-16 00:51:00 +0900

    fix desigh

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="whole">
+      <div class="navbar">
+        <p class="name">Tatsumi Tamura</p>
+        <br />
+        <div class="line">----</div>
+        <router-link :to="{ name: 'Contact' }" class="profile">Profile</router-link>
+        <router-link :to="{ name: 'Top' }" class="blog">Blog</router-link>
+        <router-link :to="{ name: 'Contact' }" class="archive">Archive</router-link>
+        <router-link :to="{ name: 'Top' }" class="contact">Contact</router-link>
+        <br />
+        <div class="line">----</div>
+      </div>
+      <router-view />
+    </div>
   </div>
 </template>
 
@@ -8,9 +21,42 @@
 export default {};
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+<style lang="scss" scoped>
+@mixin font {
+  font-family: serif;
+  text-decoration: none;
+  color: #000000;
+  margin-bottom: 6px;
+}
+
+.whole {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.navbar {
+  width: 15%;
+  margin: 90px 0px 0px 100px;
+  display: flex;
+  flex-direction: column;
+  font-size: 15px;
+  .name {
+    @include font;
+  }
+  .line {
+    @include font;
+  }
+  .profile {
+    @include font;
+  }
+  .blog {
+    @include font;
+  }
+  .archive {
+    @include font;
+  }
+  .contact {
+    @include font;
+  }
 }
 </style>
--- app/javascript/packs/component/top.vue
+++ app/javascript/packs/component/top.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="whole">
-    <div id="app">
-      <p>{{ HelloVue }}</p>
-      <img :src="TopImage" />
-      <router-link :to="{ name: 'Contact' }">連絡先へ</router-link>
+  <div id="app">
+    <div class="container">
+      <div class="image_container">
+        <img :src="TopImage" class="topImage" />
+      </div>
+      <div class="text_area">
+        <p class="introduce_text">
+          Tatsumi Tamura was born in Yamaguchi Prefecture in 1995,
+          <br />He plays tennis ball in High School Students,
+          <br />And got the right for National convention and lost second round,
+          <br />But that experience a lot of influence for him aspect a lot,
+          <br />Now he working as a engineer to make web system for B to B and B to C,
+        </p>
+      </div>
     </div>
+    <!-- <router-link :to="{ name: 'Contact' }">連絡先へ</router-link> -->
   </div>
 </template>
 
@@ -12,20 +22,53 @@
 export default {
   data: function() {
     return {
-      HelloVue: "Hello Vue!",
-      TopImage: require("../../../static/images/myImage.png")
+      TopImage: require("../../../static/images/profile_image.png")
     };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.whole {
-  width: 100%;
+@mixin font {
+  font-family: serif;
+  text-decoration: none;
+  color: #000000;
+  margin-bottom: 6px;
 }
 
-p {
-  font-size: 2em;
-  text-align: center;
+.container {
+  margin: 100px 0px 0px 0px;
+}
+
+.image_container {
+  position: relative;
+
+  .topImage {
+    height: 260px;
+    width: 370px;
+  }
+}
+
+.image_container::after {
+  position: absolute;
+  content: "";
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-shadow: inset 0px 0px 4px 4px #eeeeee;
+}
+
+.text_area {
+  margin: 25px 0px 0px 0px;
+  .introduce_text {
+    @include font();
+  }
+}
+
+@media screen and (max-width: 960px) {
+}
+
+@media screen and (max-width: 560px) {
 }
 </style>
